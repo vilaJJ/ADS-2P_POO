c1a27e3a6b0736a1985d972832ae250ce1b80b04
Juan Felipe Alves Flores <vilaJJ@users.noreply.github.com>
2023-09-14 21:44:00 -0300

    Iniciada questão 6 da lista 4 de POO
    Aula do dia 11/09/2023 (2023-09-11)

---
+++ Aula_2023-09-11/Lista4_ClassesObjetos/src/main/java/dev/mandevilla/lista4_classesobjetos/Classes/Questao6/CalculoIRRF.java
@@ -0,0 +1,45 @@
+/*
+6. A declaração de imposto de renda deve ser enviada anualmente para a Receita Federal. 
+Faça um algoritmo em Java que calcula a alíquota, com base na tabela, a ser descontado do salário de uma pessoa física. 
+Entre com o salário da pessoa física e escreva, no navegador, o salário bruto e o salário líquido, já com o desconto da alíquota. 
+Dica: utilize Classe, métodos e objetos.
+
+Base de cálculo mensal em R$            Alíquota %              Parcela a deduzir do imposto em R$
+Até 1.787,77                            Isento                  0
+De 1.787,78 até 2.679,29                7,5                     134,08
+De 2.679,30 até 3.572,43                15,0                    335,03
+De 3.572,44 até 4.463,81                22,5                    602,96
+Acima de 4.463,81                       27,5                    826,15
+ */
+package dev.mandevilla.lista4_classesobjetos.Classes.Questao6;
+
+/**
+ * @author Juan Felipe Alves Flores
+ * @since 2023-09-14
+ */
+public class CalculoIRRF {
+    private final float minValue;
+    private final float maxValue;
+    private final float aliquota;
+    private final float taxa;
+    
+    public CalculoIRRF(float minValue, float maxValue, float aliquota, float taxa){
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+        this.aliquota = aliquota;
+        this.taxa = taxa;
+    }
+
+    public float getMinValue() {
+        return minValue;
+    }
+    public float getMaxValue() {
+        return maxValue;
+    }
+    public float getAliquota() {
+        return aliquota;
+    }
+    public float getTaxa() {
+        return taxa;
+    }
+}
